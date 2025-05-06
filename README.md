1c889c36cee9ed9e08144e4338989acc8329e893
Gian.Nanquil <giannanquil17@gmail.com>
2025-05-06 16:30:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# in_between
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/in_between/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.in_between
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
