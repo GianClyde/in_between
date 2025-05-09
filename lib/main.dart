176ee42c433625a5fef3e41042b9f9ab949e2fb3
Ghost <rachellann344@gmail.com>
2025-05-09 15:03:03 +0800

    main saved

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:in_between/core/model/user_model.dart';
+import 'package:in_between/core/routes/app_router.dart';
+import 'package:in_between/features/authentication/domain/auth_repo.dart';
+import 'package:in_between/features/authentication/presentation/bloc/auth_bloc.dart';
+import 'package:in_between/features/registration/domain/registration_repo.dart';
+import 'package:in_between/features/registration/presentation/bloc/registration_bloc.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(UserModelAdapter());
+  await Hive.openBox<UserModel>('registrationBox');
+
+  final authRepo = Authrepo();
+  final registerRepo = RegistrationRepo();
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AuthBloc(authRepo)..add(GetUser())),
+        BlocProvider(
+          create: (context) => RegistrationBloc(registerRepo)..add(LoadUsers()),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'In Between',
+      routerConfig: router,
+      theme: ThemeData(
+        textTheme: GoogleFonts.girassolTextTheme().apply(
+          bodyColor: Colors.white,
         ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      // home: SafeArea(child: CashInOutScreen()),
     );
   }
 }
